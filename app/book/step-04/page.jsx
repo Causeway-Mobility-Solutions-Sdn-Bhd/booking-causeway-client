923a3c21ed99e018ebabd384b8866c4dc52c13ff
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2025-09-27 01:27:13 +0500

    push

--- app/book/step-04/page.jsx
+++ app/book/step-04/page.jsx
@@ -130,11 +130,11 @@ function Page() {
       <div className="!pb-32 py-[20px] mt-[70px] sm:mt-[90px] sm:py-[30px] max-w-[1400px] mx-auto w-[92%]">
         <div className="mt-[10px] flex justify-start items-start gap-5 flex-col lg:flex-row">
           <div className="flex-1 w-full">
-            <GuestLoginToggler
+            {/* <GuestLoginToggler
               setIsDrawerOpen={setIsLoginDrawer}
               activeTab={activeTab}
               setActiveTab={setActiveTab}
-            />
+            /> */}
 
             {/* Customer Form */}
             <CustomerDetailsForm
