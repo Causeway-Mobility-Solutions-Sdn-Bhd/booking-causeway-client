132437f9b6faded085330c81866b81dd5731c8ff
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2025-09-28 20:16:14 +0500

    update

--- components/custom/Car.jsx
+++ components/custom/Car.jsx
@@ -1,13 +1,27 @@
 "use client";
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Bookmark } from "lucide-react";
 import Image from "next/image";
 import SmartImage from "./SmartImage";
+import { addDays, format } from "date-fns";
+import hqApi from "@/lib/hqApi";
+import { useDispatch } from "react-redux";
+import {
+  setAdditionalCharges,
+  setReservation,
+  setSelectedVehicle,
+  setSelectedVehicleClasses,
+} from "@/store/slices/reservationSlice";
+import { useRouter } from "next/navigation";
+import Spinner from "./Spinner";
 
 function Car({ car }) {
   const [favorites, setFavorites] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const dispatch = useDispatch();
+  const router = useRouter();
 
   const toggleFavorite = (carId) => {
     setFavorites((prev) =>
@@ -15,6 +29,60 @@ function Car({ car }) {
         ? prev.filter((id) => id !== carId)
         : [...prev, carId]
     );
+  };
+  console.log(format(addDays(new Date(), 2), "yyyy-MM-dd"))
+
+  const fetchData = async (car) => {
+    setLoader(true);
+    try {
+      const requestData = {
+        pick_up_date: format(addDays(new Date(), 1), "yyyy-MM-dd"),
+        pick_up_time: "10:00",
+        return_date: format(addDays(new Date(), 2), "yyyy-MM-dd"),
+        return_time: "10:00",
+        pick_up_location: 1,
+        return_location: 1,
+        brand_id: 1,
+        vehicle_class_id: car?.vehicle_class?.id,
+        sort_by: "lowToHigh",
+        isCreate: true,
+        isEdit: false,
+      };
+
+      const response = await hqApi.post(
+        "car-rental/reservations/dates",
+        requestData
+      );
+
+      if (response?.status == 200) {
+        if (response?.data?.VehicleClasses?.length > 0) {
+          const reservation = response?.data?.reservation;
+          const sessionIDNew = reservation?._id;
+
+          localStorage.setItem("ssid", sessionIDNew);
+          dispatch(setSelectedVehicleClasses(response?.data?.VehicleClasses));
+
+          const responseAdditionCharges = await hqApi.get(
+            "car-rental/reservations/additional-charges",
+            { params: requestData }
+          );
+
+          if (response?.status === 200) {
+            dispatch(setReservation(responseAdditionCharges?.data?.reservation));
+            dispatch(setAdditionalCharges(responseAdditionCharges?.data?.additional_charges));
+            dispatch(setSelectedVehicle(responseAdditionCharges?.data?.selected_vehicle));
+            setLoader(false);
+            console.log("routed")
+            router.push(`/book/step-03?ssid=${sessionIDNew}`);
+          } else {
+            setLoader(false);
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      setLoader(false);
+    }
   };
 
   return (
@@ -99,8 +167,17 @@ function Car({ car }) {
         </div>
 
         {/* Book Button */}
-        <Button className="w-full h-10 sm:h-12 cursor-pointer bg-cPrimary  text-white font-semibold">
-          Book now
+        <Button
+          onClick={() => fetchData(car)}
+          className="w-full h-10 sm:h-12 cursor-pointer bg-cPrimary  text-white font-semibold"
+        >
+          {loader ? (
+            <Spinner size={20} color="#fff" thickness={3} />
+          ) : (
+            <>
+              <span className="-mt-1">Book</span>
+            </>
+          )}
         </Button>
       </CardContent>
     </Card>
