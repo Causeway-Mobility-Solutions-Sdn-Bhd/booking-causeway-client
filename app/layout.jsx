088f598544b4d15e92350703c9a72e676f2e6333
Mohamed Lafir umair <99977258+DevoUmair@users.noreply.github.com>
2025-10-03 23:20:56 +0500

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className={`${poppins.variable} antialiased`}>
         <Providers>
-          <RefreshTokenLoader />
+          {/* <RefreshTokenLoader /> */}
           {children}
           <BlackBg />
           <Toaster
