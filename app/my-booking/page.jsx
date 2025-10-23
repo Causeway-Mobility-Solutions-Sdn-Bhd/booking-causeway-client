ab6fcef0f768b381c6958c81498c8baee25b23f7
Minthajamr <minthajameer@gmail.com>
2025-10-23 20:37:45 +0530

    My booking update

---
+++ app/my-booking/page.jsx
@@ -0,0 +1,9 @@
+import BookingList from './_components/Booking List';
+
+export default function BookingListPage() {
+  return (
+    <div className="relative min-h-screen bg-gray-50" style={{backgroundColor: '#f9fafb'}}>
+      <BookingList />
+    </div>
+  );
+}
