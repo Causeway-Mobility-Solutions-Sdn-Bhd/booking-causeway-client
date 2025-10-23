0f7643cab4a8cdc3d2c94a2ccb333fe7a3af94f6
Minthajamr <minthajameer@gmail.com>
2025-10-23 20:38:11 +0530

    Update page.jsx

--- app/manage/page.jsx
+++ app/manage/page.jsx
@@ -1,17 +1,9 @@
-import SideBar from '../_components/SideBar';
-import Nav from '../_components/Nav';
-import ManageBookingC from './_components/ManageBookingC';
+import ManageBookingC from './_components/Find Booking';
 
 export default function ManageBookingPage() {
   return (
     <div className="relative min-h-screen bg-gray-50" style={{backgroundColor: '#f9fafb'}}>
-      <Nav isMain={false} value="My Booking"/>
-      <SideBar />
-      <div className="pt-5 pb-10 px-4">
-        <div className="max-w-7xl mx-auto">
-          <ManageBookingC type="primary" />
-        </div>
-      </div>
+      <ManageBookingC type="primary" />
     </div>
   );
 }
