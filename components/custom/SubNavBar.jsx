b58ea5d7b2d6015e831219c8e42c3293196c1bd0
unknown <saimfayyaz124@gmail.com>
2025-11-06 00:53:15 +0500

    Back Button Manage Booking

--- components/custom/SubNavBar.jsx
+++ components/custom/SubNavBar.jsx
@@ -1,11 +1,16 @@
 import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-function SubNavBar({name}) {
+function SubNavBar({ name }) {
+  const router = useRouter();
+  const onClick = () => {
+    router.replace("/");
+  };
   return (
     <div className="max-w-7xl mx-auto px-4 h-16 flex items-center justify-center relative border-b border-gray-200 bg-white">
       <button
-        onClick={() => window.history.back()}
+        onClick={onClick}
         className="absolute left-0 p-2 w-10 h-10 flex items-center justify-center rounded-lg hover:bg-gray-100 transition-colors"
       >
         <ChevronLeft className="w-7 h-7 text-[#2DBDB6]" strokeWidth={3} />
